3e63e6ec7ccd7bd100042365cdadaa068703606a
Heath Wang <yishu.jay@gmail.com>
2018-01-22 13:19:13 +0800

    Initial Commit

---
+++ RandomPay/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RandomPay
+//
+//  Created by Heath on 22/01/2018.
+//  Copyright © 2018 heathwang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RandomPay/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RandomPay
+//
+//  Created by Heath on 22/01/2018.
+//  Copyright © 2018 heathwang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
